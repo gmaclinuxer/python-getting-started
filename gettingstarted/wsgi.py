0416821bc56447391d72f633fa8d24b5d4ec3fd3
bksaas <bksaas@coding.net>
2018-02-05 08:52:07 +0800

    test only

--- gettingstarted/wsgi.py
+++ gettingstarted/wsgi.py
@@ -13,5 +13,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
+print 'test only'
+
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
